3cb8c4394e50f372dd1a3d2b787a1292ea152e84
Robert Bunch <rob@digitalcrafts.com>
2016-05-16 14:03:28 -0400

    Ad checks to see if we are at the end of beginning

--- students.php
+++ students.php
@@ -6,18 +6,47 @@ DB::$password = 'x';
 DB::$dbName = 'phpsandbox';
 DB::$host = '127.0.0.1';
 
+$numberOfStudents = $_GET['numberOfStudents'];
+if(isset($numberOfStudents)){
+	$limit = " LIMIT ".$numberOfStudents;
+}else{
+	$limit = " LIMIT 0";
+}
+
+//Query to get only the students we need
+$students = DB::query("SELECT * FROM students".$limit);
+
+//Query to get the total number of students in the DB
+DB::query("SELECT * FROM students");
+$total_students = DB::count();
+
+foreach($students as $student){
+	print $student['name'];
+	print "<hr />";
+}
+
+$addNumberOfStudents = $numberOfStudents+1;
+$subNumberOfStudents = $numberOfStudents-1;
+?>
+
+<?php
+if($numberOfStudents < $total_students): ?>
+<a href="students.php?numberOfStudents=<?php print $addNumberOfStudents; ?>"><button type="submit">Click to add a student</button></a>
+<?php endif; ?>
+
+<?php 
+if($numberOfStudents > 0): ?>
+<a href="students.php?numberOfStudents=<?php print $subNumberOfStudents; ?>"><button type="submit">Click to remove a student</button></a>
+<?php endif; ?>
+
+
+<?php
+
 // Make an array in php of all students in the class
 
 // $students = array('Tristan', 'Josh', 'Bogdan', 'Lazlo', 'Keith', 'Will', 'Curtis', 'Joe', 'Kochan', 'Patrick', 'Jonathan', 'Jeremy');
 
-$students = array('student'=>'Tristan', 'Josh', 'Bogdan', 'student2'=>'Lazlo', 'Keith', 'Will', 'Curtis', 'Joe', 'Kochan', 'Patrick', 'Jonathan', 'Jeremy');
-
-$numberOfStudents = $_GET['numberOfStudents'];
-$students = DB::query("SELECT * FROM students");
-for($i=0; $i<$numberOfStudents; $i++){
-	print $students[$i]['name'];
-	print "<hr />";
-}
+// $students = array('student'=>'Tristan', 'Josh', 'Bogdan', 'student2'=>'Lazlo', 'Keith', 'Will', 'Curtis', 'Joe', 'Kochan', 'Patrick', 'Jonathan', 'Jeremy');
 
 
 // foreach($students as $student){
@@ -129,13 +158,6 @@ for($i=0; $i<$numberOfStudents; $i++){
 // array_push($students6, 'Tristan', 'josh', 'Patrick');
 
 // array_push($students6, 'student1'=>'Tristan', 'student2'=>'josh');
-
-
-?>
-
-
-	<a href="students.php?numberOfStudents=0"><button type="submit">Click to add a student</button></a>
-
 
 
 
